3c33be9e6ac83119ce3d061440c3d6c73c10f644
saifulislammunna <saifulmunna28@gmail.com>
2021-09-21 19:41:08 +0600

    rest-countries added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+ 
+import { useEffect, useState } from 'react';
 import './App.css';
+import Countries from './components/Countries/Countries';
+ 
+ 
+ 
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
+       
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+function Countries(){
+ const [countries, setCountries] =  useState([]);
+
+ useEffect(() => {
+
+   fetch('https://restcountries.eu/rest/v2/all')
+   .then(res => res.json())
+   .then(data => setCountries(data));
+ }, [])
+  return(
+
+    <div>
+      <h2>Travelling around the world!!</h2>
+      <h4>Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <Country name={country.name} capital ={country.capital}></Country>)
+      }
+    </div>
+  )
+}
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Capital: {props.capital}</p>
+    </div>
+  )
+} */
 
 export default App;
